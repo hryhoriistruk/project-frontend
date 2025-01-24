3b036293f7f1020d77d7ce70a79d0049df21442d
Hryhorii_Struk <136825012+hryhoriistruk@users.noreply.github.com>
2025-01-24 19:05:43 +0200

    Update WebConfig.java

--- src/main/java/com/game/config/WebConfig.java
+++ src/main/java/com/game/config/WebConfig.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationContextAware; 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
